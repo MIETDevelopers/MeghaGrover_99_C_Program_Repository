203ccbb166bb0913ea37106108885873d85ddacf
megha07648 <76222199+megha07648@users.noreply.github.com>
2021-03-17 00:54:56 +0530

    arthmetic operator

--- 4_calculator.c
+++ 4_calculator.c
@@ -1,20 +1,4 @@
-int main() {
-    char operator;
-    double n1, n2;
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&n1, &n2);
-    switch(operator)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
-            break; // this statement is correct syntaxt wise
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
-   
-// Program to create a simple calculator
-#include <stdio.h>
+#include<stdio.h> 
 
 int main() {
     char operator;
@@ -30,7 +14,7 @@ int main() {
             break; // this statement is correct syntaxt wise
         case '-':
             printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
-        case '*':
+    case '*':
             printf("%.1lf * %.3lf = %.2lf",n1, n2, n1*n2);
         case '/':
             printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
