b52450de157f301d5a7571f04acba2cb96214bc8
megha07648 <76222199+megha07648@users.noreply.github.com>
2021-03-18 21:36:58 +0530

    added comments to hello world

--- 3_hello.c
+++ 3_hello.c
@@ -1,43 +1,9 @@
-int main() {
-    char operator;
-    double n1, n2;
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&n1, &n2);
-    switch(operator)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
-            break; // this statement is correct syntaxt wise
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
-   
-// Program to create a simple calculator
-#include <stdio.h>
+//Name: Megha Grover
+//Program:Hello World program
+//Roll No. 99
+#include <stdio.h> ////Pre-process directive to include standard input and output functions header file
 
-int main() {
-    char operator;
-    double n1, n2;
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&n1, &n2);
-    switch(operator)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
-            break; // this statement is correct syntaxt wise
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
-        case '*':
-            printf("%.1lf * %.3lf = %.2lf",n1, n2, n1*n2);
-        case '/':
-            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
-            break;
-        // operator doesn't match any case constant +, -, *, /
-        default:
-            printf("Error! operator is not correct");
-    }
-    return 0;
+int main(){      //Main Function
+  printf("Hello World\n");   //Printf function calling (to print hello world)
+  return 0; //return function
 }
