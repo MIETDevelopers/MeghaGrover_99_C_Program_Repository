dacec375833f7da2d24223e0f649d2269c1a65de
megha07648 <76222199+megha07648@users.noreply.github.com>
2021-03-18 22:21:41 +0530

    added comments in vowel program

--- 8_Vowel.c
+++ 8_Vowel.c
@@ -1,27 +1,31 @@
-#include <stdio.h>
+//Megha Grover
+//Program: Even or odd alphabets
+#include <stdio.h>    //Pre-process directive to include standard input and output functions header file
 
-int main()
+int main()            //main function
 {
-char ch;
-printf("Enter character\n");
-scanf("%c",&ch);
-switch(ch)
-{
-   case'a':
-   case'e':
-   case'i':
-   case'o':
-   case'u':
-   case'A':
-   case'E':
-   case'I':
-   case'O':
-   case'U':
-   printf("%c is a vowel",ch);
-   break;
-   default:
-   printf("%c is a consonant",ch);
-   break;
-}   
-return 0;
+   char ch;      //variable declaration with data type char
+   printf("Enter character\n");  //printf function calling
+   scanf("%c",&ch);        //scanf function calling
+   //variable definition using switch case for both uppercase and lowercase variable "c"
+   switch(ch)
+   {
+       case'a':
+       case'e':
+       case'i':
+       case'o':
+       case'u':
+       case'A':
+       case'E':
+       case'I':
+       case'O':
+       case'U':
+       printf("%c is a vowel",ch);
+       break;
+       //if variable doesn't match any case variable 
+       default:
+       printf("%c is a consonant",ch);
+       break;
+   }   
+    return 0; //return function 
 }
