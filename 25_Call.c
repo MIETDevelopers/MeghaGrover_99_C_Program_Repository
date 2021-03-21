36a893d4ebf4b7f757e22cbb6a3e92b9b7e5063c
megha07648 <76222199+megha07648@users.noreply.github.com>
2021-03-21 18:40:19 +0530

    value by call

--- 25_Call.c
+++ 25_Call.c
@@ -1,25 +1,19 @@
 //Name: Megha Grover
-//Program:to swap two numbers using call by reference            
-
-#include <stdio.h>      //Pre-process directive to include standard input and output functions header file
- void swap(int*, int*);
- int main(){
-   int x, y;
-   //Printf and scanf function calling
-   printf("Enter the value of x and y\n");
-   scanf("%d%d",&x,&y);
-   printf("Before Swapping\nx = %d\ny = %d\n", x, y);
-      swap(&x, &y); 
-   printf("After Swapping\nx = %d\ny = %d\n", x, y);
- 
-   return 0;
-}
- 
-void swap(int *a, int *b)
-{
-   int temp;
-   // Here swapping occurs.
-   temp = *b;
-   *b = *a;
-   *a = temp;   
+//Program: Swap two numbers using call by value. ()        
+#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+  
+int main() {        //main function
+    int x, y; 
+    printf("Enter Value of x "); 
+    scanf("%d", &x); 
+    printf("\nEnter Value of y "); 
+    scanf("%d", &y); 
+    
+    //Here swapping of number occurs.
+    int temp = x; 
+    x = y; 
+    y = temp; 
+  
+    printf("\nAfter Swapping: x = %d, y = %d", x, y); 
+    return 0;   //return function
 }
