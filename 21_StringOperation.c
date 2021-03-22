0ed566aca08d23b7ccb3e98cd4ed70af995b7a92
megha07648 <76222199+megha07648@users.noreply.github.com>
2021-03-22 13:02:15 +0530

    Array of structure for student marks

---
+++ 21_StringOperation.c
@@ -0,0 +1,19 @@
+//Name: Megha Grover
+//Program: Read the string in pointers loaction and give it as output.			
+
+#include <stdio.h>		//Pre-process directive to include standard input and output functions header file
+
+int main(void) {
+  char name[] = "Ishav Verma";
+
+  printf("%c \n", *name);     	// Output will be I
+  printf("%c \n", *(name+3));	// Output will be A
+  printf("%c \n", *(name+8));   // Output will be R
+
+  char *namePtr;
+
+  namePtr = name;
+  printf("%c \n", *namePtr);     // Output will be I
+  printf("%c \n", *(namePtr+2)); // Output will be H
+  printf("%c \n", *(namePtr+7)); // Output will be E
+}
