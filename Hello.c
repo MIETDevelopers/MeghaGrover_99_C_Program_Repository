1d4e7912254988a0f42b76b260f884192fe89060
megha07648 <76222199+megha07648@users.noreply.github.com>
2021-03-28 18:09:19 +0530

    Q4

---
+++ Hello.c
@@ -0,0 +1,9 @@
+//Name: Megha Grover
+//Program:Hello World program
+//Roll No. 99
+#include <stdio.h> ////Pre-process directive to include standard input and output functions header file
+
+int main(){      //Main Function
+  printf("Hello World\n");   //Printf function calling (to print hello world)
+  return 0; //return function
+}
